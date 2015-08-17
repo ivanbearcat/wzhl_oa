cbbc7a13cc80b47c90aca81b494e733c53df1ee3
root <root@localhost.(none)>
2015-08-17 10:21:05 +0800

    fixbug

--- main/views.py
+++ main/views.py
@@ -8,6 +8,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def main(request):
     path = request.path.split('/')[1]
-    return render_to_response('public/index.html',{'user':request.user.username,
+    return render_to_response('public/index.html',{'user':'%s%s' % (request.user.last_name,request.user.first_name),
                                                    'path1':path,
                                                    'page_name1':u'主页'})
