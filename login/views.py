6aeb05498e380a938e13929016909bbcce0cb6fe
root <root@localhost.(none)>
2015-10-27 15:30:25 +0800

    fix next page bug

--- login/views.py
+++ login/views.py
@@ -15,11 +15,17 @@ def login_auth(request):
     authed = auth.authenticate(username=user_auth,password=passwd_auth)
     if authed and authed.is_active:
         auth.login(request,authed)
-        if globals().has_key('next_next') and not next_next == None:
-            logger.info('<%s> login in sucess.' % user_auth)
-            return HttpResponseRedirect(next_next)
+        # if globals().has_key('next_next') and not next_next == None:
+        #     logger.info('<%s> login in sucess.' % user_auth)
+        #     return HttpResponseRedirect(next_next)
+        # else:
+        #     logger.info('<%s> login in sucess.' % user_auth)
+        #     return HttpResponseRedirect('/main/')
+        next_page = request.session.get('next')
+        if next_page:
+            request.session.pop('next')
+            return HttpResponseRedirect(next_page)
         else:
-            logger.info('<%s> login in sucess.' % user_auth)
             return HttpResponseRedirect('/main/')
     else:
         logger.warn('<%s> login in fail.' % user_auth)
@@ -31,8 +37,9 @@ def logout(request):
 
 def not_login(request):
     next_next = request.GET.get('next')
-    print next_next
-    global next_next
+    # print next_next
+    # global next_next
+    request.session['next'] = next_next
     return render_to_response('login/login.html',{'msg':u'您还没有登录'})
 
 def test_notify(request):
