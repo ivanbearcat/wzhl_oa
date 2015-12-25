2d6931a77956de9ea34b8ef3e58a8f3d990c1823
root <root@localhost.(none)>
2015-12-25 11:38:28 +0800

    fix access /admin/ bug

--- wzhl_oa/settings.py
+++ wzhl_oa/settings.py
@@ -182,7 +182,7 @@ LOGGING = {
  }
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-   # 'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
    # 'django.core.context_processors.debug',
    # 'django.core.context_processors.i18n',
