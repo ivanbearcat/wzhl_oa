1672b74e6bf8e9995bf7e0a30eff38d991e003b6
root <root@localhost.(none)>
2016-01-04 10:43:03 +0800

    add comment

--- libs/refresh_script.py
+++ libs/refresh_script.py
@@ -1,6 +1,14 @@
 #coding:utf-8
 import requests,sys
 
+###加入crontab
+
+# #结算资产价值
+# 0 5 25 * * /usr/bin/python /usr/share/nginx/wzhl_oa/libs/refresh_script.py assets_refresh
+#
+# #刷新假日
+# 0 10 * * * /usr/bin/python /usr/share/nginx/wzhl_oa/libs/refresh_script.py vacation_refresh
+
 username = 'refresh_user'
 password = 'MTFkNzJkYjkyM2Ji'
 
