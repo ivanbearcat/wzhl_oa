4335135e667f321a197ccd9c2885ad276717aabc
root <root@localhost.(none)>
2016-01-04 10:29:56 +0800

    fix bug

--- libs/refresh_script.py
+++ libs/refresh_script.py
@@ -11,8 +11,8 @@ def refresh(path):
     payload = {'username':username,'password':password}
     #print payload
     headers = {'content-type':'application/json'}
-    r = s.post('http://192.168.100.251/login_auth/',data=payload,headers=headers)
+    r = s.post('http://oa.xiaoquan.com/login_auth/',data=payload,headers=headers)
     cookies = r.cookies
-    s.post('http://192.168.100.251:8000/%s/' % path,cookies=cookies)
+    s.post('http://oa.xiaoquan.com/%s/' % path,cookies=cookies)
 
 refresh(path)
