e5ec4fd194af67122e567e1fcb9ea40386bb4c55
root <root@localhost.(none)>
2016-03-14 20:56:07 +0800

    开发了部分人员信息功能

--- personal_information/views.py
+++ personal_information/views.py
@@ -4,11 +4,10 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.db.models.query_utils import Q
 from django.contrib.auth.decorators import login_required
-from libs.sendmail import send_mail
 from personal_information.models import table
-from vacation.models import user_table
 from wzhl_oa.settings import BASE_DIR
 import json
+import datetime
 
 @login_required
 def personal_information_table(request):
@@ -88,9 +87,68 @@ def personal_information_table_data(request):
 @login_required
 def personal_information_table_detail(request):
     path = request.path.split('/')[1]
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    keywords = request.POST.get('keywords')
+    sex = request.POST.get('sex')
+    birthday = request.POST.get('birthday')
+    graduate_date = request.POST.get('graduate_date')
+    first_education = request.POST.get('first_education')
+    education = request.POST.get('education')
+    specialty = request.POST.get('specialty')
+    graduate_school = request.POST.get('graduate_school')
+    last_company = request.POST.get('last_company')
+    channel = request.POST.get('channel')
+    referrer = request.POST.get('referrer')
+    salary = request.POST.get('salary')
+    job_level = request.POST.get('job_level')
+    job_title = request.POST.get('job_title')
+    job_type = request.POST.get('job_type')
+    resumes = request.FILES.get('resumes')
+    direction = request.POST.get('direction')
+    resource_state = request.POST.get('resource_state')
+    comment = request.POST.get('comment')
+    executor = request.POST.get('executor')
+    _id = request.POST.get('id')
+
+    if _id:
+        orm = table.objects.get(id=_id)
+        if orm.resource_state != resource_state:
+            orm.state_change_time = datetime.datetime.now()
+        orm.name = name
+        orm.email = email
+        orm.phone = phone
+        orm.keywords = keywords
+        orm.sex = sex
+        orm.birthday = birthday
+        orm.graduate_date = graduate_date
+        orm.first_education = first_education
+        orm.education = education
+        orm.specialty = specialty
+        orm.graduate_school = graduate_school
+        orm.last_company = last_company
+        orm.channel = channel
+        orm.referrer = referrer
+        orm.salary = salary
+        orm.job_level = job_level
+        orm.job_title = job_title
+        orm.job_type = job_type
+        orm.resumes = resumes
+        orm.direction = direction
+        orm.resource_state = resource_state
+        orm.comment = comment
+        orm.executor = executor
+    else:
+        orm.object
+
+
+
+
     return render(request, 'personal_information/personal_information_table_detail.html',{'user':'%s%s' % (request.user.last_name,request.user.first_name),
                                                  'path1':'personal_information',
                                                  'path2':path,
                                                  'page_name1':u'人员信息',
                                                  'page_name2':u'人员基本信息表',},
                                                 context_instance=RequestContext(request))
+
